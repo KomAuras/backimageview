20ad2a9a972ecec8f816da8ff958cd0e98d17e25
Evgeny Stefanenko <mixer@clarionlife.net>
2017-09-17 12:34:23 +1000

    stick preview to right bottom

--- js/backimageview.js
+++ js/backimageview.js
@@ -1,27 +1,42 @@
 this.backimageview = function(){
+    //console.log('backimageview');
     xOffset = 100;
     yOffset = 150;
     $("div.s-image a img").hover(function(e){
         var image = this.src.replace(/48x48/gi,'750x0');
         image = image.replace(/200x0/gi,'750x0');
         $("body").append("<p id='pluginBackPreview'><img src='"+ image +"'/></p>");
+        var scrollYpos = $(document).scrollTop();
         $("#pluginBackPreview")
-            .css("top",(e.pageY - xOffset) + "px")
-            .css("left",(e.pageX + yOffset) + "px")
+            .css("bottom",-scrollYpos)
+            .css("right","0px")
             .fadeIn("fast");
     },
     function(){
         $("#pluginBackPreview").remove();
     });
     $("div.s-image a img").mousemove(function(e){
+        var scrollYpos = $(document).scrollTop();
         $("#pluginBackPreview")
-            .css("top",(e.pageY - xOffset) + "px")
-            .css("left",(e.pageX + yOffset) + "px");
+            .css("bottom",-scrollYpos)
+            .css("right","0px");
     });
+    setTimeout(function() {
+        backimageview();
+    }, 1000);    
 };
 
 $(document).ready(function(){
+    //console.log('ready');
     setTimeout(function() {
         backimageview();
-    }, 2000);    
+    }, 1000);    
+});
+
+$(window).scroll(function() {
+    var scrollYpos = $(document).scrollTop();
+    $("#pluginBackPreview").css({
+        'bottom': -scrollYpos,
+        'position': 'absolute'
+    });
 });
